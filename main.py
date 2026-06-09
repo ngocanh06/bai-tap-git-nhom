7eed505b843c8218341d3ede4ffba71e1861f988
Hanh <hahoanganh1082005@gmail.com>
2026-06-10 00:34:11 +0700

    feat: dev B cap nhat thue muoi phan tram

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ def tinh_thue_thu_nhap(thu_nhap):
     #TODO: Sinh viên sẽ cập nhật công thức tính thuế ở đây
   return thu_nhap*0.1 
   #Dev B áp dụng mức thuế 10%
-print("Thuế phải nộp của bạn là: ", tinh_thue_thu_nhap(10000000))
+
